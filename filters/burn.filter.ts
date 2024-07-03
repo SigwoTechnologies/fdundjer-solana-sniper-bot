d39265299e952c7d9e89333871d2f7e0f7dd0a9f
TomasAgustin95 <jcarlosblanco4@gmail.com>
2024-07-03 20:46:56 -0300

    update burn.filter.ts

--- filters/burn.filter.ts
+++ filters/burn.filter.ts
@@ -5,10 +5,10 @@ import { logger, BURN_AMOUNT } from '../helpers';
 import BN from 'bn.js';
 export class BurnFilter implements Filter {
   private cachedResult: FilterResult | undefined = undefined;
-  oldAmount: BN;
+  oldAmount: number;
 
   constructor(private readonly connection: Connection) {
-    this.oldAmount = new BN(0);
+    this.oldAmount = 0;
   }
 
   async execute(poolKeys: LiquidityPoolKeysV4): Promise<FilterResult> {
@@ -21,13 +21,14 @@ export class BurnFilter implements Filter {
       // const burned = amount.value.uiAmount === 0;
       // logger.debug(`burned: ${amount.value.uiAmount}SOL`);
       // logger.trace({ lpmint: poolKeys.lpMint, uiAmount: amount.value.uiAmount });
+      // console.log(poolKeys);
       const solAmount =
         amount.value.uiAmount || amount.value.uiAmount == 0
-          ? new BN(amount.value.uiAmount).divn(LAMPORTS_PER_SOL / 10 ** 4)
-          : new BN(Number(amount.value.amount)).divn((LAMPORTS_PER_SOL / 10 ** 4) * amount.value.decimals);
-      const burned = this.oldAmount.sub(solAmount).gtn(BURN_AMOUNT);
+          ? amount.value.uiAmount / (LAMPORTS_PER_SOL / 10 ** 4)
+          : Number(amount.value.amount) / ((LAMPORTS_PER_SOL / 10 ** 4) * amount.value.decimals);
+      const burned = this.oldAmount - solAmount > BURN_AMOUNT;
       logger.debug(
-        `burned: ${burned}, total: ${solAmount}SOL, burned: ${this.oldAmount.sub(solAmount)}SOL, BurnAmount: ${BURN_AMOUNT}, uiAmount: ${amount.value.uiAmount}, decimals: ${amount.value.decimals}`,
+        `burned: ${burned}, total: ${solAmount}SOL, burned: ${this.oldAmount - solAmount}SOL, BurnAmount: ${BURN_AMOUNT}, uiAmount: ${amount.value.uiAmount}, decimals: ${amount.value.decimals}`,
       );
       this.oldAmount = solAmount;
       const result = { ok: burned, message: burned ? undefined : "Burned -> Creator didn't burn LP" };
