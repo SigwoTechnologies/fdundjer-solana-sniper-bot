763080ebcf9c83fa81ff1a838d217be0f4aaa19c
TomasAgustin95 <jcarlosblanco4@gmail.com>
2024-07-02 22:46:02 -0300

    update burn.filter.ts

--- filters/burn.filter.ts
+++ filters/burn.filter.ts
@@ -2,7 +2,6 @@ import { Filter, FilterResult } from './pool-filters';
 import { Connection, LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { LiquidityPoolKeysV4 } from '@raydium-io/raydium-sdk';
 import { logger, BURN_AMOUNT } from '../helpers';
-
 export class BurnFilter implements Filter {
   private cachedResult: FilterResult | undefined = undefined;
   oldAmount: number;
@@ -19,15 +18,13 @@ export class BurnFilter implements Filter {
     try {
       const amount = await this.connection.getTokenSupply(poolKeys.lpMint, this.connection.commitment);
       // logger.trace({ amount, lpmint: poolKeys.lpMint });
-      const lamportAmount = amount.value.uiAmount
-        ? amount.value.uiAmount * 10 ** amount.value.decimals
-        : Number(amount.value.amount);
+      const solAmount = amount.value.uiAmount
+        ? (10 ** amount.value.decimals / LAMPORTS_PER_SOL) * amount.value.uiAmount
+        : Number(amount.value.amount) / LAMPORTS_PER_SOL;
       // const burned = amount.value.uiAmount === 0 && this.oldAmount > BURN_AMOUNT;
-      const burned = this.oldAmount - lamportAmount > BURN_AMOUNT * LAMPORTS_PER_SOL;
-      logger.debug(
-        `total: ${lamportAmount / LAMPORTS_PER_SOL}SOL, burned: ${(this.oldAmount - lamportAmount) / LAMPORTS_PER_SOL}SOL`,
-      );
-      this.oldAmount = lamportAmount;
+      const burned = this.oldAmount - solAmount > BURN_AMOUNT;
+      logger.debug(`burned: ${burned}, total: ${solAmount}SOL, burned: ${this.oldAmount - solAmount}SOL`);
+      this.oldAmount = solAmount;
       const result = { ok: burned, message: burned ? undefined : "Burned -> Creator didn't burn LP" };
 
       if (result.ok) {
